a5dbb23486db0cb52cc02166c5c0de2c99ea5eb7
Steve Gordon <sgordon@hotmail.co.uk>
2019-05-17 14:05:01 +0100

    Adding empty benchmarking project

---
+++ src/Middleware/ResponseCaching/perf/ResponseCompressionProviderBenchmark.cs
@@ -0,0 +1,18 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
+
+namespace Microsoft.AspNetCore.ResponseCompression.Benchmarks
+{
+    public class ResponseCachingKeyProviderBenchmark
+    {
+        
+    }
+}

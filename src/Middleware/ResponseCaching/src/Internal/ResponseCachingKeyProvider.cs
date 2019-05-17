617f4dcc0b0abc4a256942194cf06f1c77bfbf0a
Steve Gordon <sgordon@hotmail.co.uk>
2019-05-17 14:05:27 +0100

    WIP - optimised key provider

--- src/Middleware/ResponseCaching/src/Internal/ResponseCachingKeyProvider.cs
+++ src/Middleware/ResponseCaching/src/Internal/ResponseCachingKeyProvider.cs
@@ -5,12 +5,202 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.ObjectPool;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
 
 namespace Microsoft.AspNetCore.ResponseCaching.Internal
 {
+    internal class ResponseCachingKeyProviderV2 : IResponseCachingKeyProvider
+    {
+        // Use the record separator for delimiting components of the cache key to avoid possible collisions
+        private static readonly char KeyDelimiter = '\x1e';
+
+        // Use the unit separator for delimiting subcomponents of the cache key to avoid possible collisions
+        private static readonly char KeySubDelimiter = '\x1f';
+
+        private static readonly char HChar = 'H';
+
+        private readonly ResponseCachingOptions _options;
+
+        internal ResponseCachingKeyProviderV2(IOptions<ResponseCachingOptions> options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            _options = options.Value;
+        }
+
+        public IEnumerable<string> CreateLookupVaryByKeys(ResponseCachingContext context)
+        {
+            yield return CreateStorageVaryByKey(context);
+        }
+
+        // GET<delimiter>SCHEME<delimiter>HOST:PORT/PATHBASE/PATH
+        public string CreateBaseKey(ResponseCachingContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var request = context.HttpContext.Request;
+
+            var keyLength = CalculateKeyLength();
+
+            var baseKey = string.Create(keyLength, request, (chars, httpRequest) =>
+            {
+                var position = 0;
+
+                httpRequest.Method.AsSpan().ToUpperInvariant(chars);
+                position += httpRequest.Method.Length;
+
+                chars[position++] = KeyDelimiter;
+
+                httpRequest.Scheme.AsSpan().ToUpperInvariant(chars.Slice(position));
+                position += httpRequest.Scheme.Length;
+
+                chars[position++] = KeyDelimiter;
+
+                httpRequest.Host.Value.AsSpan().ToUpperInvariant(chars.Slice(position));
+                position += httpRequest.Host.Value.Length;
+
+                var pathBaseSpan = httpRequest.PathBase.Value.AsSpan();
+                var pathSpan = httpRequest.Path.Value.AsSpan();
+
+                if (_options.UseCaseSensitivePaths)
+                {
+                    pathBaseSpan.ToUpperInvariant(chars.Slice(position));
+                    position += pathBaseSpan.Length;
+
+                    pathSpan.ToUpperInvariant(chars.Slice(position));
+                }
+                else
+                {
+                    pathBaseSpan.CopyTo(chars.Slice(position));
+                    position += pathBaseSpan.Length;
+
+                    pathSpan.CopyTo(chars.Slice(position));
+                }
+            });
+
+            return baseKey;
+
+            int CalculateKeyLength() =>
+                request.Method.Length +
+                1 + // char for KeyDelimiter
+                request.Scheme.Length +
+                1 + // char for KeyDelimiter
+                request.Host.Value.Length +
+                request.PathBase.Value.Length +
+                request.Path.Value.Length;
+        }
+
+        // BaseKey<delimiter>H<delimiter>HeaderName=HeaderValue<delimiter>Q<delimiter>QueryName=QueryValue1<subdelimiter>QueryValue2
+        public string CreateStorageVaryByKey(ResponseCachingContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var varyByRules = context.CachedVaryByRules;
+            if (varyByRules == null)
+            {
+                throw new InvalidOperationException(
+                    $"{nameof(CachedVaryByRules)} must not be null on the {nameof(ResponseCachingContext)}");
+            }
+
+            var hasHeaders = !StringValues.IsNullOrEmpty(varyByRules.Headers);
+            var hasQueryKeys = !StringValues.IsNullOrEmpty(varyByRules.QueryKeys);
+
+            if (!hasHeaders && !hasQueryKeys)
+            {
+                return varyByRules.VaryByKeyPrefix;
+            }
+
+            var request = context.HttpContext.Request;
+
+            var keyLength = varyByRules.VaryByKeyPrefix.Length;
+
+            int headersCount, queryKeysCount;
+
+            // calculate header length
+            if (hasHeaders && (headersCount = varyByRules.Headers.Count) > 0) // might not need the count. Can StringValues be non-null/empty + have 0 items?
+            {
+                keyLength += 2; // chars for KeyDelimiter and group separator for the header segment of the cache key
+
+                var requestHeaders = context.HttpContext.Request.Headers;
+
+                for (var i = headersCount - 1; i >= 0; i++) // does this limit a bounds check?
+                {
+                    var header = varyByRules.Headers[i];
+                    var headerValues = requestHeaders[header];
+
+                    keyLength += header.Length + 2; // includes delimiter, header key and equals sign.
+
+                    for (var j = headerValues.Count - 1; j >= 0; j--) // does this limit a bounds check?
+                    {
+                        keyLength += headerValues[j].Length;
+                    }
+                }
+            }
+
+            bool specialKey; // naming!
+
+            // calculate query keys length
+            if (hasQueryKeys && (queryKeysCount = varyByRules.QueryKeys.Count) > 0) // might not need the count. Can StringValues be non-null/empty + have 0 items?
+            {
+                keyLength += 2; // chars for KeyDelimiter and group separator for the header segment of the cache key
+
+                specialKey = varyByRules.QueryKeys.Count == 1 &&
+                             string.Equals(varyByRules.QueryKeys[0], "*", StringComparison.Ordinal);
+
+                if (specialKey)
+                {
+                    //var queryCount = context.HttpContext.Request.Query.Count;
+
+                    foreach (var (key, value) in context.HttpContext.Request.Query) // foreach here as it's string indexed
+                    {
+                        keyLength += key.Length + 2; // includes delimiter, query key and equals sign.
+
+                        for (var i = value.Count - 1; i >= 0; i--) // does this limit a bounds check?
+                        {
+                            keyLength += value[i].Length;
+                            if (i != 0) keyLength++;
+                        }
+                    }
+                }
+                else
+                {
+                    for (var i = queryKeysCount - 1; i >= 0; i++) // does this limit a bounds check?
+                    {
+                        var queryKey = varyByRules.QueryKeys[i];
+                        var queryKeyValues = context.HttpContext.Request.Query[queryKey];
+
+                        keyLength += queryKey.Length + 2; // includes delimiter, header key and equals sign.
+
+                        for (var j = queryKeyValues.Count - 1; j >= 0; j--) // does this limit a bounds check?
+                        {
+                            keyLength += queryKeyValues[j].Length;
+                            if (j != 0) keyLength++;
+                        }
+                    }
+                }
+            }
+
+            var varyByKey = string.Create(keyLength, varyByRules, (chars, rules) =>
+            {
+                // TODO
+            });
+
+            return varyByKey;
+        }
+    }
+
     internal class ResponseCachingKeyProvider : IResponseCachingKeyProvider
     {
         // Use the record separator for delimiting components of the cache key to avoid possible collisions
